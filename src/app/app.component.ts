898222d98b62ca2b83213ee8e3e37a42403935b1
Agend047 <schneiderniklas@gmx.de>
2023-12-24 16:47:04 +0100

    Few last things before Christmas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,34 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component'
+
 import { MainComponent } from './main/main.component';
+
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
 import { ImprintComponent } from './imprint/imprint.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent, MainComponent, ImprintComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AboutMeComponent,
+    ContactComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    ImprintComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
