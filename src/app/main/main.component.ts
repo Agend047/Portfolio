f61b570355e063a7b44b410ccd59e9beb5381673
Agend047 <schneiderniklas@gmx.de>
2024-01-01 18:33:02 +0100

    Had to write the same function. Not good!

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
+  emailstring = "mailto:schneiderniklas@gmx.de?Subject=Hello&body=links:  %0D http://link1.com  %0D http://link1.com";
+
+  goToLink(url: string) {
+    window.open(url, "_blank")
+  }
+
 }
+
