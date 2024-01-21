0492f3546b8e88a7baa33dfc93f724db611543f4
Agend047 <schneiderniklas@gmx.de>
2024-01-21 20:15:32 +0100

    Lots of try and errors, fix save-backup

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormControl, FormBuilder, FormsModule } from "@angular/forms";
+
+import { CustomCheckboxComponent } from '../custom-checkbox/custom-checkbox.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    CustomCheckboxComponent,
+    MatCheckboxModule,
+    FormsModule,
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  inputOne = true;
+
+  form;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      myFormControl: true
+    });
+
+  }
 }
