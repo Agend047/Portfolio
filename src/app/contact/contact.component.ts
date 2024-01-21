6f32194741b78d93bb22fd33bde908d4a31df66e
Agend047 <schneiderniklas@gmx.de>
2024-01-21 22:24:18 +0100

    Made fit for first uploading. Cant upload on my own server, so no contact formular for now

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -2,22 +2,23 @@ import { Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormControl, FormBuilder, FormsModule } from "@angular/forms";
 
-import { CustomCheckboxComponent } from '../custom-checkbox/custom-checkbox.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [
-    CustomCheckboxComponent,
     MatCheckboxModule,
     FormsModule,
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
+
+
 export class ContactComponent {
 
-  inputOne = true;
+
+  readPolicy: boolean = false;
 
   form;
 
@@ -25,6 +26,10 @@ export class ContactComponent {
     this.form = this.formBuilder.group({
       myFormControl: true
     });
+
+  }
+
+  onSubmit(stuff: object) {
 
   }
 }
